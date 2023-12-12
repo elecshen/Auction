d50605a68702f991a67b511ecc56d7ecfc98ea4b
elecshen <51473599+elecshen@users.noreply.github.com>
2023-12-12 12:56:35 +0700

    Добавлен  интерфейс сервиса обработки куки

---
+++ Auction/Services/Coockie/ICoockieService.cs
@@ -0,0 +1,11 @@
+﻿namespace Auction.Services.Coockie
+{
+    public interface ICoockieService
+    {
+        public void RefreshCoockie(HttpContext context);
+
+        public void AddCoockie(HttpContext context);
+
+        public void RemoveCoockie(HttpContext context);
+    }
+}
