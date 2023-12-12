b8fea6282f46b171e673dacc3f1492ff944c74a2
elecshen <51473599+elecshen@users.noreply.github.com>
2023-12-12 16:47:44 +0700

    Изменён интерфейс сервиса управления куки и создан частично реализованный класс

---
+++ Auction/Services/Coockie/CoockieService.cs
@@ -0,0 +1,44 @@
+﻿using Auction.Models.ConstModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Auction.Models.ConstModels.CoockieEnums;
+
+namespace Auction.Services.Coockie
+{
+    public class CoockieService : ICoockieService
+    {
+        public void AddCoockie(HttpContext context, CoockieBase coockieProperty, string value)
+        {
+            context.Response.Cookies.Append(coockieProperty.Key, value, new() { MaxAge=TimeSpan.FromDays(30)});
+        }
+
+        public string? GetCoockie(HttpContext context, CoockieBase coockieProperty)
+        {
+            string? v = null;
+            bool valueExist = 
+                context.Request.Cookies.ContainsKey(coockieProperty.Key) 
+                && context.Request.Cookies.TryGetValue(coockieProperty.Key, out v) 
+                && v is not null;
+            if (coockieProperty.Key == ThemeObject.Key)
+            {
+                if (valueExist && ThemeObject.Values.Contains(v!))
+                    return v;
+                AddCoockie(context, coockieProperty, ThemeObject.Default);
+                return ThemeObject.Default;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void RefreshCoockie(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCoockie(HttpContext context, CoockieBase coockieProperty)
+        {
+            context.Response.Cookies.Delete(coockieProperty.Key);
+        }
+    }
+}
--- Auction/Services/Coockie/ICoockieService.cs
+++ Auction/Services/Coockie/ICoockieService.cs
@@ -1,11 +1,15 @@
-﻿namespace Auction.Services.Coockie
+﻿using static Auction.Models.ConstModels.CoockieEnums;
+
+namespace Auction.Services.Coockie
 {
     public interface ICoockieService
     {
+        public void AddCoockie(HttpContext context, CoockieBase coockieProperty, string value);
+
+        public string? GetCoockie(HttpContext context, CoockieBase coockieProperty);
+
         public void RefreshCoockie(HttpContext context);
 
-        public void AddCoockie(HttpContext context);
-
-        public void RemoveCoockie(HttpContext context);
+        public void RemoveCoockie(HttpContext context, CoockieBase coockieProperty);
     }
 }
